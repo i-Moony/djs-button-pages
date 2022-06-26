f51fbdc0edef52512695eea88ebdece73771f7d2
m00ny <danila.kononov.official@gmail.com>
2022-06-27 00:11:00 +0300

    More human readable jsdoc

--- src/Typings/ButtonAction.ts
+++ src/Typings/ButtonAction.ts
@@ -2,6 +2,8 @@ import PaginationData from "../Classes/Paginations/Abstract/PaginationData";
 
 /**
  * Type to represent button action.
+ * 
+ * May be either a number of a page or a function that determines number of a page.
  */
 type ButtonAction =
 |   number
--- src/Typings/ButtonDisableWhen.ts
+++ src/Typings/ButtonDisableWhen.ts
@@ -1,7 +1,9 @@
 import PaginationData from "../Classes/Paginations/Abstract/PaginationData";
 
 /**
- * Type to represent when button should be disabled.
+ * Type to represent condition of disabling button.
+ * 
+ * May be either a number of a page or a function that determines number of a page.
  */
 type ButtonDisableWhen =
 |   number
