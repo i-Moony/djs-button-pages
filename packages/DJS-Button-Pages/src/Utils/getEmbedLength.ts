cc57495dca3296e6eb7ddd3d8eeddd82f4461d66
Danila Kononov <danila.kononov.official@gmail.com>
2022-11-23 10:32:41 +0300

    chore getEmbedLength: move to new folders

---
+++ packages/DJS-Button-Pages/src/Utils/getEmbedLength.ts
@@ -0,0 +1,23 @@
+import { APIEmbed } from "discord.js";
+
+/**
+ * Gets embed length.
+ * Includes author name, footer, fields, description and title.
+ * @param {APIEmbed} embed Embed which length should be counted.
+ * @returns {number} Embed length.
+ */
+function getEmbedLength(embed:APIEmbed): number
+{
+    return (embed.author?.name.length ?? 0) +
+    (embed.footer?.text?.length ?? 0) +
+    (embed.fields ? 
+        embed.fields.length >= 1 
+            ? embed.fields.reduce((prev, curr) => prev + curr.name.length + curr.value.length, 0)
+            : 0
+        : 0
+    ) +
+    (embed.description?.length ?? 0) + 
+    (embed.title?.length ?? 0);
+};
+
+export default getEmbedLength;
