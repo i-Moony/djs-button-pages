d5a756ed3c87355cb7377210387f2a2f72399328
Danila Kononov <danila.kononov.official@gmail.com>
2022-11-23 12:00:40 +0300

    refactor Presets: make new package
    Now pre-made buttons are a part of own package.

---
+++ packages/Presets/src/Presets.ts
@@ -0,0 +1,14 @@
+import FirstPageButton from "./Buttons/FirstPageButton";
+import LastPageButton from "./Buttons/LastPageButton";
+import NextPageButton from "./Buttons/NextPageButton";
+import PreviousPageButton from "./Buttons/PreviousPageButton";
+import StopButton from "./Buttons/StopButton";
+
+export
+{
+    FirstPageButton,
+    LastPageButton,
+    NextPageButton,
+    PreviousPageButton,
+    StopButton
+};
