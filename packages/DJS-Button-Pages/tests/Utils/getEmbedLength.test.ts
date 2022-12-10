3c476955f5d0e4a339434f660ec203dcaba43d63
m00ny <danila.kononov.official@gmail.com>
2022-12-11 00:03:29 +0300

    chore: fix tests' names

--- packages/DJS-Button-Pages/tests/Utils/getEmbedLength.test.ts
+++ packages/DJS-Button-Pages/tests/Utils/getEmbedLength.test.ts
@@ -2,7 +2,7 @@ import { EmbedBuilder } from "discord.js";
 import { getEmbedLength } from "../../src/Paginations";
 
 describe("getEmbedLength: function that counts embed's length.", () => {
-    test("correctly count embed's length", () => {
+    test("correctly count embed's length.", () => {
         const name = "Sample nickname.",
         description = "Sample description.",
         fieldName = "Sample field.",
@@ -25,7 +25,7 @@ describe("getEmbedLength: function that counts embed's length.", () => {
         expect(getEmbedLength(embed.data)).toBe(name.length + description.length + fieldName.length + fieldValue.length + text.length + title.length);
     });
 
-    test("correctly count empty embed's length", () => {
+    test("correctly count empty embed's length.", () => {
         const embed = new EmbedBuilder();
 
         expect(getEmbedLength(embed.data)).toBe(0);
