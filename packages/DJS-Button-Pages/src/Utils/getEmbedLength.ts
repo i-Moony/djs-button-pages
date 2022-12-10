98749d23d457f5a668f3f337b7cde36ec6b92a95
m00ny <danila.kononov.official@gmail.com>
2022-12-10 21:11:23 +0300

    refactor: simplify fields count

--- packages/DJS-Button-Pages/src/Utils/getEmbedLength.ts
+++ packages/DJS-Button-Pages/src/Utils/getEmbedLength.ts
@@ -11,9 +11,7 @@ function getEmbedLength(embed:APIEmbed): number
     return (embed.author?.name.length ?? 0) +
     (embed.footer?.text?.length ?? 0) +
     (embed.fields ? 
-        embed.fields.length >= 1 
-            ? embed.fields.reduce((prev, curr) => prev + curr.name.length + curr.value.length, 0)
-            : 0
+        embed.fields.reduce((prev, curr) => prev + curr.name.length + curr.value.length, 0)
         : 0
     ) +
     (embed.description?.length ?? 0) + 
